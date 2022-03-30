a5ea2be2356807bc38db0dc0d2f8de8952f19085
신승민 <sinseungmin@sinseungmin-ui-MacBookPro-2.local>
2022-03-30 18:28:55 +0900

    JS 변환완료

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import { useState } from 'react';
-import './App.css';
+import React, { useState } from 'react';
 import Board from './Board';
+import './App.css';
 
 const App = () => {
+  
 
-  return (
-    <div className="App">
-      <h2>Tic - Tac - Toe</h2>
-      <Board />
-    </div>
-  );
+    return(
+        <>
+          <div className='App'>
+          <h2>Tic - Tac - Toe</h2>
+            <Board />
+            </div>
+        </>
+    )
+    
 }
 
-export default App;
+export default App
--- src/Board.js
+++ src/Board.js
@@ -1,39 +1,80 @@
 import React, { useEffect, useState } from 'react';
 import Button from './Button';
+
 import './Board.css'
 
 const Board = () => {
-    // 3x3 테이블을 만든다, 
-    const [squares, setSquares] = useState(Array(9).fill(null));
-    // console.log(squares)
+    const [square, setSquare] = useState(Array(9).fill(null));
+    const [stepNumber, setStepNumber] = useState(0);
+    const [xIsNext, setXIsNext] = useState(true);
+
+    const handleClick = (n) => {
+        const sliceSq = square.slice();
+        if(calculateWinner(square) || square[n]) return;
+        sliceSq[n] = xIsNext ? "X" : "O"
+        setSquare(sliceSq)
+        setXIsNext(!xIsNext)
+    };
 
     const renderSquare = (n) => {
-        for(let i = 0; i < squares.length; i++) {
-            squares[i] = "X"
-        }
-    }
-    renderSquare();
+        return (
+            <>
+                <Button
+                    vlaue={square[n]}
+                    onCick={() => handleClick(n)}
+                />
+            </>
+        )
+    };
 
+    const calculateWinner = (squares) => {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+                return squares[a];
+            }
+        }
+        return null;
+    };
+
+    const winner = calculateWinner(square);
+    let status;
+    if (winner) {
+        status = 'Winner: ' + winner;
+    } else {
+        status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+    }
 
     return (
-        <>
-            <div className='board_row'>
-                <Button squares={squares[0]} />
-                <Button squares={squares[1]} />
-                <Button squares={squares[2]} />
+        <div>
+            <div className='status'>{status}</div>
+            <div className="board-row">
+                {renderSquare(0)}
+                {renderSquare(1)}
+                {renderSquare(2)}
             </div>
-            <div className='board_row'>
-                <Button squares={squares[3]} />
-                <Button squares={squares[4]} />
-                <Button squares={squares[5]} />
+            <div className="board-row">
+                {renderSquare(3)}
+                {renderSquare(4)}
+                {renderSquare(5)}
             </div>
-            <div className='board_row'>
-                <Button squares={squares[6]} />
-                <Button squares={squares[7]} />
-                <Button squares={squares[8]} />
+            <div className="board-row">
+                {renderSquare(6)}
+                {renderSquare(7)}
+                {renderSquare(8)}
             </div>
-        </>
-    )
+        </div>
+    );
 }
 
 export default Board;
