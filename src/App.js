c5e6401eb212d53c0615a65046c58e93a3e23dfb
신승민 <sinseungmin@sinseungmin-ui-MacBookPro-2.local>
2022-03-30 21:39:54 +0900

    start

--- src/App.js
+++ src/App.js
@@ -3,7 +3,37 @@ import Board from './Board';
 import './App.css';
 
 const App = () => {
-  const [history, setHistory] = useState(Array(9).fill(null))
+  const [square, setSquare] = useState(Array(9).fill(null));
+  const [stepNumber, setStepNumber] = useState(0);
+  const [xIsNext, setXIsNext] = useState(true);
+
+  const handleClick = (n) => {
+    const sliceSq = square.slice();
+    if(calculateWinner(square) || square[n]) return;
+    sliceSq[n] = xIsNext ? "X" : "O"
+    setSquare(sliceSq)
+    setXIsNext(!xIsNext)
+};
+
+const calculateWinner = (squares) => {
+  const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+          return squares[a];
+      }
+  }
+  return null;
+};
 
     return(
         <>
--- src/Board.js
+++ src/Board.js
@@ -4,17 +4,9 @@ import Button from './Button';
 import './Board.css'
 
 const Board = () => {
-    const [square, setSquare] = useState(Array(9).fill(null));
-    const [stepNumber, setStepNumber] = useState(0);
-    const [xIsNext, setXIsNext] = useState(true);
+    
 
-    const handleClick = (n) => {
-        const sliceSq = square.slice();
-        if(calculateWinner(square) || square[n]) return;
-        sliceSq[n] = xIsNext ? "X" : "O"
-        setSquare(sliceSq)
-        setXIsNext(!xIsNext)
-    };
+    
 
     const renderSquare = (n) => {
         return (
@@ -27,25 +19,7 @@ const Board = () => {
         )
     };
 
-    const calculateWinner = (squares) => {
-        const lines = [
-            [0, 1, 2],
-            [3, 4, 5],
-            [6, 7, 8],
-            [0, 3, 6],
-            [1, 4, 7],
-            [2, 5, 8],
-            [0, 4, 8],
-            [2, 4, 6],
-        ];
-        for (let i = 0; i < lines.length; i++) {
-            const [a, b, c] = lines[i];
-            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-                return squares[a];
-            }
-        }
-        return null;
-    };
+    
 
     const winner = calculateWinner(square);
     let status;
